bb74fc3abea044bf29ddb2c58e948cead961d94a
Shayne Preston <shayne.preston@protonmail.com>
2024-11-11 14:19:51 -0500

    feat(playwright): setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { getPlaywrightEnv } from 'playwright/env/playwright-env';
+
+const playwrightEnv = getPlaywrightEnv();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -16,19 +12,20 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  forbidOnly: playwrightEnv.CI,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: '50%',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list', { printSteps: true }],
+    ['html', { open: 'never', outputFolder: 'playwright-report' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: playwrightEnv.URL,
+    ignoreHTTPSErrors: true,
     trace: 'on-first-retry',
   },
 
@@ -71,9 +68,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm run dev', // using vite dev server for https
+    url: playwrightEnv.URL,
+    reuseExistingServer: !playwrightEnv.USE_LOCAL_SERVER,
+    ignoreHTTPSErrors: true,
+  },
 });
