2efc90d064358f62f438c25d81b18b46b173ba0d
Shayne Preston <shayne.preston@protonmail.com>
2024-11-29 23:00:17 -0500

    feat(remix-island): setup

--- src/remix-app/root.tsx
+++ src/remix-app/root.tsx
@@ -1,6 +1,6 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Outlet } from '@remix-run/react';
 
-import { useNonce } from './hooks/nonce';
+import { Document } from './Document';
 import stylexStylesheet from './main.css?url';
 
 import type { LinksFunction } from '@remix-run/node';
@@ -12,27 +12,11 @@ const links: LinksFunction = () => [
   },
 ];
 
-const App = () => {
-  const nonce = useNonce();
-
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-
-      <body>
-        <Outlet />
-
-        <ScrollRestoration nonce={nonce ?? undefined} />
-        <Scripts nonce={nonce ?? undefined} />
-      </body>
-    </html>
-  );
-};
+const App = () => (
+  <Document>
+    <Outlet />
+  </Document>
+);
 
 export default App;
 export { links };
